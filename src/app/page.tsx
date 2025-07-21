786ccd826fdfe0c73c7f58b0cb75c300d06cc7d7
Ryan Yogan <ryanyogan@gmail.com>
2025-07-21 16:47:41 -0500

    Adds trpc for client and server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Button>I am a button</Button>
+      <h1>Hello Again</h1>
     </div>
   );
 }
