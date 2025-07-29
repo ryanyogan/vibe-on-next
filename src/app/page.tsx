22b46c1c7ee9c20869ccb7bfcc7f9fe1d16056a1
Ryan Yogan <ryanyogan@gmail.com>
2025-07-28 19:44:48 -0500

    it puts the forgotten file in the repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default async function Home() {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions());
+  const [value, setValue] = useState("");
+
   return (
-    <div>
-      <h1>Hello Again</h1>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({ value })}>Invoke Function</Button>
     </div>
   );
 }
