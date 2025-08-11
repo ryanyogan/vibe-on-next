107ac8cb8010c6a7a55494741685dca877680a4a
Ryan Yogan <ryanyogan@gmail.com>
2025-08-11 16:13:11 -0500

    Now we are scrolling, heh

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -4,14 +4,30 @@ import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { MessageCard } from "./message-card";
 import { MessageForm } from "./message-form";
+import { useEffect, useRef } from "react";
 
 export function MessagesContainer({ projectId }: { projectId: string }) {
   const trpc = useTRPC();
+  const bottomRef = useRef<HTMLDivElement>(null);
   const { data: messages } = useSuspenseQuery(
     trpc.messages.getMany.queryOptions({
       projectId,
     }),
   );
+
+  useEffect(() => {
+    const lastAssistentMessage = messages.findLast(
+      (message) => message.role === "ASSISTANT",
+    );
+
+    if (lastAssistentMessage) {
+      // TODO :Set the active fragment
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView();
+  }, [messages.length]);
 
   return (
     <div className="flex flex-col flex-1 min-h-0">
@@ -29,10 +45,12 @@ export function MessagesContainer({ projectId }: { projectId: string }) {
               type={message.type}
             />
           ))}
+          <div ref={bottomRef} />
         </div>
       </div>
 
       <div className="relative p-3 pt-1">
+        <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background/70 pointer-events-none" />
         <MessageForm projectId={projectId} />
       </div>
     </div>
